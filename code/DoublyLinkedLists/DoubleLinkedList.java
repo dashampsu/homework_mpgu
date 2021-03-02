0ce41aa9b63df46ccb1843e1a689fe3bda189d92
dashampsu <neykuratick@mail.ru>
2021-03-03 02:18:14 +0300

    list-append

--- code/DoublyLinkedLists/DoubleLinkedList.java
+++ code/DoublyLinkedLists/DoubleLinkedList.java
@@ -16,4 +16,17 @@ public class DoubleLinkedList {
         head.next = newNode;
         head = newNode;
     }
+
+    public void append(Object data) {
+        DoubleLinkedListElement newNode = new DoubleLinkedListElement(tail, data, null);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+
+        tail.prev = newNode;
+        head = newNode;
+    }
 }
