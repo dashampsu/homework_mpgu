edfa90c324fd76ab2005e1b0c4702d8c5a0e4d47
dashampsu <neykuratick@mail.ru>
2021-03-03 02:17:08 +0300

    list-insert

--- code/DoublyLinkedLists/DoubleLinkedList.java
+++ code/DoublyLinkedLists/DoubleLinkedList.java
@@ -1,4 +1,19 @@
 package DoublyLinkedLists;
 
 public class DoubleLinkedList {
+    DoubleLinkedListElement head;
+    DoubleLinkedListElement tail;
+
+    public void insert(Object data) {
+        DoubleLinkedListElement newNode = new DoubleLinkedListElement(null, data, head);
+
+        if (head == null) {
+            head = newNode;
+            tail = newNode;
+            return;
+        }
+
+        head.next = newNode;
+        head = newNode;
+    }
 }
