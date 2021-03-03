b9f5e3aebe4e77bb7d1056842310c4875a85cb54
dashampsu <neykuratick@mail.ru>
2021-03-03 11:57:12 +0300

    tests

---
+++ code/DoublyLinkedLists/DoubleLinkedListTests.java
@@ -0,0 +1,12 @@
+package DoublyLinkedLists;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class DoubleLinkedListTests extends Assert {
+
+    @Test
+    public void test() {
+        System.out.println(1);
+    }
+}
