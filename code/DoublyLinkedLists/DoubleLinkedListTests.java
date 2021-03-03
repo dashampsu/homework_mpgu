0e2fdd32943ca523d7720ef8b18f91fa87a4abac
dashampsu <neykuratick@mail.ru>
2021-03-03 12:01:59 +0300

    TEST list_init_listSize

--- code/DoublyLinkedLists/DoubleLinkedListTests.java
+++ code/DoublyLinkedLists/DoubleLinkedListTests.java
@@ -6,7 +6,22 @@ import org.junit.Test;
 public class DoubleLinkedListTests extends Assert {
 
     @Test
-    public void test() {
-        System.out.println(1);
+    public void list_init_listSize() {
+        DoubleLinkedList listy = new DoubleLinkedList();
+        int expected = 0;
+
+        int actual = listy.size;
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void list_insert_listSize() {
+        DoubleLinkedList listy = new DoubleLinkedList();
+        int expected = 1;
+
+        listy.insert(1);
+
+        int actual = listy.size;
+        assertEquals(expected, actual);
     }
 }
