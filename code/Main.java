a88d39cade6ab45776b4bfe41df4fb5abd0a66f8
dashampsu <neykuratick@mail.ru>
2021-03-05 05:12:16 +0300

    Tast 3 complete

--- code/Main.java
+++ code/Main.java
@@ -4,18 +4,6 @@ import DoublyLinkedLists.DoubleLinkedListElement;
 public class Main {
 
     public static void main(String[] args) {
-        DoubleLinkedList listy = new DoubleLinkedList();
 
-        listy.append(1); // 1
-        listy.append(2); // 1 2
-        listy.append(3); // 1 2 3
-
-        listy.printAll(); // 3 2 1 starting from end
-        DoubleLinkedListElement node = listy.findFirstNodeByValue(3);
-        DoubleLinkedListElement newnode = new DoubleLinkedListElement(22);
-
-        listy.insertBefore(node, 22);
-
-        listy.printAll();
     }
 }
