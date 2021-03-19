b1453cfefaaee76e17628299bd7adb0ed58f2ead
dashampsu <neykuratick@mail.ru>
2021-03-19 08:28:57 +0300

    dequeue

--- code/Queue/Queue.java
+++ code/Queue/Queue.java
@@ -10,14 +10,12 @@ public class Queue<T> {
         size++;
     }
 
+    public void dequeue() {
+        queue.remove(0);
+        size--;
+    }
 
-//    DoubleLinkedList<T> queue = new DoubleLinkedList<T>();
-//    int size = 0;
-//
-//    public void enqueue(T value) {
-//        queue.append(value);
-//        size++;
-//    }
+
 //
 //    public void dequeue() {
 //        DoubleLinkedListElement<T> nodeToSet = queue.getNodeByIndex(1);
