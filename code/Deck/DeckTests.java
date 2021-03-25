8841b40f35518697e6454823fac98b4850edee25
dashampsu <neykuratick@mail.ru>
2021-03-25 11:46:04 +0300

    push_tests

--- code/Deck/DeckTests.java
+++ code/Deck/DeckTests.java
@@ -27,4 +27,19 @@ public class DeckTests {
         int actual = deck.getSize();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void pushFront_dataTest() {
+        Deck<Integer> deck = new Deck<Integer>();
+
+        deck.pushFront(1);
+        deck.pushFront(2);
+        deck.pushFront(3);
+        deck.pushFront(4);
+        deck.pushFront(5);
+
+        int expected = 5;
+        int actual = deck.peekFront();
+        assertEquals(expected, actual);
+    }
 }
