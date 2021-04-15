880e1705d81dbf3a9c043af2749f2d296a1a96d1
dashampsu <neykuratick@mail.ru>
2021-04-15 19:59:35 +0300

    get successor tests

--- code/BinarySearchTree/BinarySearchTreeTests.java
+++ code/BinarySearchTree/BinarySearchTreeTests.java
@@ -67,4 +67,28 @@ public class BinarySearchTreeTests {
         String actual = tree.getMax().getKey();
         assertEquals(expected, actual);
     }
+
+    @Test
+    public void getSuccessor_positive() {
+        BinarySearchTree tree = new BinarySearchTree("abcdefgh");
+        tree.insert("abc");
+        tree.insert("abcd");
+        tree.insert("abcdefghijk");
+        tree.insert("abcdefghi");
+        tree.insert("ab");
+        tree.insert("abcdefghijklmnopqrst");
+        tree.insert("abcdefghijklmnopq");
+        tree.insert("abcde");
+        tree.insert("a");
+        tree.insert("abcdefghijklmnop");
+        tree.insert("abcdefghijklmnopqr");
+
+        String expected = "abcdefghijklmnop";
+        String actual = tree.getSuccessor(tree.search("abcdefghijk")).getKey();
+        assertEquals(expected, actual);
+
+        expected = "abcdefghi";
+        actual = tree.getSuccessor(tree.search("abcdefgh")).getKey();
+        assertEquals(expected, actual);
+    }
 }
