96998ae042a1c9ec99e8e243825cefd25b0e63d1
dashampsu <neykuratick@mail.ru>
2021-04-15 20:11:52 +0300

    BinarySearchTree Complete

--- code/BinarySearchTree/BinarySearchTreeTests.java
+++ code/BinarySearchTree/BinarySearchTreeTests.java
@@ -3,6 +3,7 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class BinarySearchTreeTests {
+    // complete
 
     @Test
     public void insertion_GeneralTest_positive() {
