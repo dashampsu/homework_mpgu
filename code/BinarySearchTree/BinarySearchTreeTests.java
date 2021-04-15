d466906af199f5ff3292ab5113a51e926775e21e
dashampsu <neykuratick@mail.ru>
2021-04-15 19:57:07 +0300

    min and max tests

--- code/BinarySearchTree/BinarySearchTreeTests.java
+++ code/BinarySearchTree/BinarySearchTreeTests.java
@@ -5,7 +5,7 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class BinarySearchTreeTests {
 
     @Test
-    public void insertionTest() {
+    public void insertionTest_positive() {
         BinarySearchTree tree = new BinarySearchTree("abcdefgh");
         tree.insert("abc");
         tree.insert("abcd");
@@ -28,8 +28,43 @@ public class BinarySearchTreeTests {
         assertEquals(expected, actual);
     }
 
+    @Test
+    public void min_test_positive() {
+        BinarySearchTree tree = new BinarySearchTree("abcdefgh");
+        tree.insert("abc");
+        tree.insert("abcd");
+        tree.insert("abcdefghijk");
+        tree.insert("abcdefghi");
+        tree.insert("ab");
+        tree.insert("abcdefghijklmnopqrst");
+        tree.insert("abcdefghijklmnopq");
+        tree.insert("abcde");
+        tree.insert("a");
+        tree.insert("abcdefghijklmnop");
+        tree.insert("abcdefghijklmnopqr");
 
+        String expected = "a";
+        String actual = tree.getMin().getKey();
+        assertEquals(expected, actual);
+    }
 
+    @Test
+    public void max_test_positive() {
+        BinarySearchTree tree = new BinarySearchTree("abcdefgh");
+        tree.insert("abc");
+        tree.insert("abcd");
+        tree.insert("abcdefghijk");
+        tree.insert("abcdefghi");
+        tree.insert("ab");
+        tree.insert("abcdefghijklmnopqrst");
+        tree.insert("abcdefghijklmnopq");
+        tree.insert("abcde");
+        tree.insert("a");
+        tree.insert("abcdefghijklmnop");
+        tree.insert("abcdefghijklmnopqr");
 
-
+        String expected = "abcdefghijklmnopqrst";
+        String actual = tree.getMax().getKey();
+        assertEquals(expected, actual);
+    }
 }
