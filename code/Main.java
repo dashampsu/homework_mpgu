ce890451577994b6f90ac867fa0659283b0241b0
dashampsu <neykuratick@mail.ru>
2021-05-20 00:21:54 +0300

    Task One Complete

--- code/Main.java
+++ code/Main.java
@@ -5,10 +5,6 @@ public class Main {
     public static void main(String[] args) {
         ListHashTable table = new ListHashTable();
 
-        table.insert(9);
-        table.insert(18);
-        table.printAll();
-        table.remove(0);
-        table.printAll();
+        // task 1
     }
 }
