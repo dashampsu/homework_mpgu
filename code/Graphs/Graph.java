566af0f77d5763cc625b77c8d78708cabae03989
dashampsu <neykuratick@mail.ru>
2021-05-21 13:45:38 +0300

    add and remove

--- code/Graphs/Graph.java
+++ code/Graphs/Graph.java
@@ -14,5 +14,30 @@ public class Graph {
         }
     }
 
+    public void add(int source, int destination) {
+        graph.get(source).add(destination);
+        graph.get(destination).add(source);
+    }
+
+    public void delete(int source, int destination)
+    {
+        for (int i = 0; i < graph.get(destination).getsize(); i++)
+        {
+            if (graph.get(destination).get(i).equals(source))
+            {
+                graph.get(destination).remove(i);
+                break;
+            }
+        }
+        for (int i = 0; i < graph.get(source).getsize(); i++)
+        {
+            if (graph.get(source).get(i) == destination)
+            {
+                graph.get(source).remove(i);
+                break;
+            }
+        }
+    }
+
 
 }
