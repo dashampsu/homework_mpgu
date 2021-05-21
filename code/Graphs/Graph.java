866a12f40bc2e902afd2ffb710dc3246b1d5b0d1
dashampsu <neykuratick@mail.ru>
2021-05-21 13:32:34 +0300

    graph init

--- code/Graphs/Graph.java
+++ code/Graphs/Graph.java
@@ -1,4 +1,18 @@
 package Graphs;
+import DynamicArrays.*;
 
 public class Graph {
+    DynamicArray<DynamicArray<Integer>> graph;
+    int nodes;
+
+    public Graph(int nodes) {
+        this.nodes = nodes;
+        graph = new DynamicArray<DynamicArray<Integer>>();
+
+        for (int i = 0; i < nodes; i++) {
+            graph.set(i, new DynamicArray<Integer>());
+        }
+    }
+
+
 }
